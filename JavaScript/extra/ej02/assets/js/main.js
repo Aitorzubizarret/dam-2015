ac9e26ef0da3e2128e55af2b89a1885aec92bf48
aitorzubizarret <aitorzubizarreta@yahoo.es>
2015-02-22 22:59:46 +0100

    Ejercicio extra 2

---
+++ JavaScript/extra/ej02/assets/js/main.js
@@ -0,0 +1,10 @@
+// Mostramos en la consola el mensaje "Hola Mundo!"
+var mensaje = "Hola Mundo!";
+console.log(mensaje);
+
+/*
+	Mostramos un segundo mensaje
+*/
+var mensaje2 = "Qué facil es incluir 'comillas simples'";
+mensaje2 += ' y "comillas dobles"';
+console.log(mensaje2);
