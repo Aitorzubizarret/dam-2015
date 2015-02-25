db6dd77419779fb6dc37c17517c4a59f9bb058d5
aitorzubizarret <aitorzubizarreta@yahoo.es>
2015-02-25 13:46:43 +0100

    Actualización

--- JavaScript/ej07/assets/js/main.js
+++ JavaScript/ej07/assets/js/main.js
@@ -9,8 +9,17 @@
 
     // Pruebas
     console.log(validarEmail() === false);
+    console.log(validarEmail("") === false);
     console.log(validarEmail("a@a.com") === true); // Letra Arroba Letra Punto Dominio
     console.log(validarEmail("aaaaaaa@aaaaaaaa.com") === true);
+    console.log(validarEmail(".") === false);
+    console.log(validarEmail(".@") === false); // Comenzando con un punto
+    console.log(validarEmail(".@.") === false); // 
+    console.log(validarEmail("@") === false); // Solo una arroba
+    console.log(validarEmail("@aaaaaaa") === false); // Comenzando con una arroba
+    console.log(validarEmail("aaaa@") === false); // Finalizando con un arroba
+    console.log(validarEmail("email") === false); // Sin arroba
+    console.log(validarEmail("aaa@@aaaa") === false); // Con más de una arroba
 
     /*
         letra 
