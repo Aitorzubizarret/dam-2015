2195b4d4e7dba7034dcfb67b7437c11ca79382aa
aitorzubizarret <aitorzubizarreta@yahoo.es>
2015-02-25 19:30:21 +0100

    Ejercicio final

---
+++ JavaScript/final/assets/app.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    document.getElementById('login').validate();
+};
+
+/*
+formulario
+nombre class="required"
+email class="required email"
+checbox class="required"
+*/
---
+++ JavaScript/final/assets/js/main.js
@@ -0,0 +1,18 @@
+HTMLFormElement.prototype.vaslidate = function() {
+    var validador = {
+        required : function() {
+
+        },
+        email : function() {
+
+        }
+    };
+
+    var validate = function() {
+
+    },
+
+    var requeridos = this.querySelectorAll('.required');
+
+    this.addEventListener('submit', validate, false);
+};
