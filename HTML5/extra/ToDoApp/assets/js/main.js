d4c193de6cac5f886823d730a6b8f76694f3c1bb
aitorzubizarret <aitorzubizarreta@yahoo.es>
2015-03-09 11:52:34 +0100

    Actualización

--- HTML5/extra/ToDoApp/assets/js/main.js
+++ HTML5/extra/ToDoApp/assets/js/main.js
@@ -1,76 +1,67 @@
 (function() {
 	// Variables globales
 	var storage = localStorage;
-	var prefix = "toDoTasks-";
+	var prefix = "toDos-";
 
 	// Capturamos los elementos que necesitamos
-	var newTaskInput = document.getElementById("taskInput");
-	var addTaskBtn = document.getElementById("addBtn");
-	var taskList = document.getElementById("taskList");
+	var newTodoInput = document.getElementById("todoInput");
+	var addTodoBtn = document.getElementById("addBtn");
+	var todoList = document.getElementById("todoList");
 	
 	// Funciones
-	var loadSavedTasks = function() {
-		console.log("Cargada");
+	var loadSavedTodos = function() {
 		for (var i = 1; i <= storage.length; i++) {
-			createTask(storage.getItem(prefix + i));
+			createTodo(storage.getItem(prefix + i));
 		};
 	};
-	var addEventToList = function(e) {
+	var addTodoToList = function(e) {
 		e.preventDefault();
-		if (newTaskInput.value.trim() != '') {
-			// Creamos un checkbox, que servirá para marcar que la tarea esta completada.
-			var newCheckbox = document.createElement("input");
-			newCheckbox.type = "checkbox";
-			newCheckbox.classList.add("delete");
-
-			// Creamos un span para meter en él la descripción de la tarea.
-			var newTaskText = document.createElement("span");
-			newTaskText.innerText = newTaskInput.value.trim();
-
-			// Creamos un nuevo elemento (li) para la lista e insertamos en él el checkbox y el span.
-			var newTask = document.createElement("li");
-			newTask.classList.add("taskDone");
-			newTask.appendChild(newCheckbox);
-			newTask.appendChild(newTaskText);
-
-			// Insertamos en el UL el nuevo elemento LI que hemos creado.
-			taskList.appendChild(newTask);
-
-			// Guardamos la tarea en el navegador
-			saveTask(newTaskInput.value);
+		if (newTodoInput.value.trim() != '') {
+			createTodoAndSave(newTodoInput.value.trim());
 
 			// Limpiamos el input para que pueda meter nuevas tareas
-			newTaskInput.value = "";
+			newTodoInput.value = "";
 		}
 	};
-	var createTask = function(taskText) {
+	var createTodo = function(todoText) {
 		// Creamos un checkbox, que servirá para marcar que la tarea esta completada.
 		var newCheckbox = document.createElement("input");
 		newCheckbox.type = "checkbox";
 		newCheckbox.classList.add("delete");
 
 		// Creamos un span para meter en él la descripción de la tarea.
-		var newTaskText = document.createElement("span");
-		newTaskText.innerText = taskText;
+		var newTodoText = document.createElement("span");
+		newTodoText.innerText = todoText;
 
 		// Creamos un nuevo elemento (li) para la lista e insertamos en él el checkbox y el span.
-		var newTask = document.createElement("li");
-		newTask.classList.add("taskDone");
-		newTask.appendChild(newCheckbox);
-		newTask.appendChild(newTaskText);
+		var newTodo = document.createElement("li");
+		newTodo.appendChild(newCheckbox);
+		newTodo.appendChild(newTodoText);
 
 		// Insertamos en el UL el nuevo elemento LI que hemos creado.
-		taskList.appendChild(newTask);
+		todoList.appendChild(newTodo);
 	};
-	var saveTask = function(taskText) {
-		storage.setItem(prefix + (storage.length+1), taskText);
+	var createTodoAndSave = function(todoText) {
+		createTodo(todoText);
+		// Guardamos la tarea en el navegador
+		saveTodo(todoText);
 	};
-	var showTaskAsDone = function() {
-		console.log("Hello");
+	var saveTodo = function(todoText) {
+		storage.setItem(prefix + (storage.length+1), todoText);
+	};
+	var toggleDoneUndone = function(e) {
+		if (e.target.type === "checkbox") {
+			var textSpan = e.target.parentNode.getElementsByTagName("span");
+			if (e.target.checked) {
+				textSpan[0].classList.add("todoDone");
+			} else {
+				textSpan[0].classList.remove("todoDone");
+			}
+		}
 	};
 
 	// Listeners
-	addTaskBtn.addEventListener("click", addEventToList, false);
-	taskList.addEventListener("click", showTaskAsDone, false);
-	window.addEventListener("load", loadSavedTasks, false);
+	addTodoBtn.addEventListener("click", addTodoToList, false);
+	todoList.addEventListener("click", toggleDoneUndone, false);
+	window.addEventListener("load", loadSavedTodos, false);
 })();
