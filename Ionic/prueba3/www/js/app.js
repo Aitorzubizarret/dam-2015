7a8cb529c8df8230aa2cc6e01a7a386018d7fc30
aitorzubizarret <aitorzubizarreta@yahoo.es>
2015-03-17 12:31:43 +0100

    Prueba de navegación en Ionic

---
+++ Ionic/prueba3/www/js/app.js
@@ -0,0 +1,17 @@
+angular.module('navegacion', ['ionic'])
+
+  .config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+    $stateProvider.state('home', {
+      url: '/',
+      templateUrl: 'template/main.html'
+    })
+    $stateProvider.state('info', {
+      url: '/info',
+      templateUrl: 'template/info.html'
+    })
+  })
+
+  .controller('principal', function() {
+    console.log("hola");
+});
